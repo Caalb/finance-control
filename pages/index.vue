42076c1951a4fa7c0ab253dbae90f16cd3984971
Caalb <carlosmunizfjunior@gmail.com>
2024-03-02 00:43:58 -0300

    feat(pages/index): adiciona tela de login

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex justify-between">
+    <div class="w-1/3">
+      <h6 class="text-2xl mb-5">
+        WELCOME BACK!
+      </h6>
+
+      <p>
+        Don't have an account?
+
+        <NuxtLink to="/signup">
+          Sign up
+        </NuxtLink>
+      </p>
+
+      <LoginForm />
+    </div>
+
+    <div class="w-3/4">
+      <img
+        src="~/assets/images/login.svg"
+        alt="Login"
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
