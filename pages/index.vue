9b8ea7e8cbb3dd5d68d7d0aef549821d44bf40f7
Mateus Ávila Isidoro <mateus@mateusavila.com.br>
2024-03-04 21:57:54 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,3 @@
-<template>
-  <div>
-    <h1>Home</h1>
-  </div>
-</template>
-
 <script setup lang="ts">
   definePageMeta({
     title: "Home",
@@ -12,3 +6,10 @@
 
   useHead({ title: "Login Page | Home" })
 </script>
+<template>
+  <!-- sempre dê um nome pra classe, mesmo que seja para não fazer nada ou deixar largura cheia, como neste caso. -->
+  <div class="w-full">
+    <h1>Home</h1>
+  </div>
+</template>
+
