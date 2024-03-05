462eea2cbcd8c8028050bbfb078f7e1811901124
Mateus Ávila Isidoro <mateus@mateusavila.com.br>
2024-03-04 21:48:53 -0300

    Update app.vue
    Deixando o código mais conciso:
    
    1. sempre deixar onMounted pro final.
    2. Ficou bem bom o resto

--- app.vue
+++ app.vue
@@ -5,23 +5,14 @@
 </template>
 
 <script type="ts" setup>
-  import { onMounted } from "vue";
-
   const colorMode = useColorMode()
-
-  onMounted(() => {
-    setThemePreference()
-  })
-
   const setThemePreference = () => {
     const theme = localStorage.getItem('theme')
-
     if (theme) {
       return colorMode.preference = theme
     }
-
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-
     return colorMode.preference = prefersDark ? 'dark' : 'light'
   }
+  onMounted(() => setThemePreference())
 </script>
