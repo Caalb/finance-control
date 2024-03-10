cc7ceab92ea3e3bf8befea16deaa5ec54b7cb028
Caalb <carlosmunizfjunior@gmail.com>
2024-03-10 00:38:49 -0300

    feat(plugins/http): adiciona plugin do ofetch

---
+++ plugins/http.ts
@@ -0,0 +1,31 @@
+import MobulaService from "~/services/mobula";
+
+interface IHttp {
+  mobula: MobulaService;
+}
+
+
+export default defineNuxtPlugin(async () => {
+  const { public: { MOBULA_API_KEY} } = useRuntimeConfig();
+  const fetcher = $fetch.create({
+    baseURL: 'https://api.mobula.io/api/1',
+    onRequest({ options }) {
+      const authHeader = { 
+        Authorization: MOBULA_API_KEY,
+        'Content-Type': 'application/json'
+      }
+      
+      options.headers = Object.assign(authHeader, options.headers)
+    }
+  })
+
+  const httpServices: IHttp = {
+    mobula: new MobulaService(fetcher)
+  }
+
+  return {
+    provide: {
+      http: httpServices
+    }
+  }
+})
