94109f3a191485218022b7ae6a7fe1207ab6accc
Mateus Ávila Isidoro <mateus@mateusavila.com.br>
2024-03-30 19:20:32 -0300

    Update auth.ts
    Add comment. 
    
    Aqui pode simplesmente rejeitar, pois é comentário. Quando aplicar o panva/jose, dai reabre ela.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,3 +1,5 @@
+// quando for usar a lib panva/jose para validar JWT, tens que adaptar ela também aqui.
+
 import { defineNuxtRouteMiddleware} from "#app";
 
 export default defineNuxtRouteMiddleware(() => {
