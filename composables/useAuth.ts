aba88b252e6c9901f05b2aa40657d798e551fefa
Mateus Ávila Isidoro <mateus@mateusavila.com.br>
2024-03-30 19:16:50 -0300

    Update useAuth.ts
    add comment

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,5 +1,7 @@
 import { useStorage } from "@vueuse/core";
 
+// considere usar o panva/jose como pacote para validar objeto JWT. Ele é excelente!
+
 export const useAuth = () => {
   const { value: { expiration}} = useStorage("auth", { expiration: "0" });
 
